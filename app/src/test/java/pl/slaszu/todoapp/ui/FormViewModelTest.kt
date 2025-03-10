9eea073b3120b51bbff0165fc8f925fab96cda41
slaszu <piotr.flasza@gmail.com>
2025-03-10 19:47:13 +0100

    tests in progress

--- app/src/test/java/pl/slaszu/todoapp/ui/FormViewModelTest.kt
+++ app/src/test/java/pl/slaszu/todoapp/ui/FormViewModelTest.kt
@@ -2,6 +2,7 @@ package pl.slaszu.todoapp.ui
 
 import androidx.compose.material3.SnackbarHostState
 import kotlinx.coroutines.test.runTest
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,6 +12,7 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import pl.slaszu.todoapp.MainDispatcherRule
@@ -41,7 +43,7 @@ class FormViewModelTest {
     private lateinit var snackbarHostState: SnackbarHostState
 
     @Test
-    fun saveWorkCorrect() {
+    fun `save item if is different then loaded one`() {
         runTest {
 
             // arrange
@@ -74,6 +76,93 @@ class FormViewModelTest {
                 withDismissAction = any(),
                 duration = any()
             )
+        }
+    }
+
+    @Test
+    fun `not save item if is the same as loaded one`() {
+        runTest {
+
+            // arrange
+            val item = FakeTodoModel(text = "item from repo")
+
+            whenever(presentationService.getStringResource(R.string.todo_form_no_change)).thenReturn(
+                "no changes"
+            )
+            whenever(todoRepo.getById(1)).thenReturn(item)
+            val callback: (TodoModel) -> Unit = mock()
+
+            val viewModel = FormViewModel(
+                todoRepository = todoRepo,
+                todoModelFactory = todoFactory,
+                presentationService = presentationService
+            )
+
+            // act
+            viewModel.loadTodoItemToEditForm(1)
+            viewModel.save(
+                item = item,
+                snackbarHostState = snackbarHostState,
+                callback = callback
+            )
+
+            // assert
+            verify(todoRepo, never()).save(any())
+            verify(callback, never()).invoke(any())
+            verify(snackbarHostState).showSnackbar(
+                message = eq("no changes"),
+                actionLabel = anyOrNull(),
+                withDismissAction = any(),
+                duration = any()
+            )
+        }
+    }
+
+    @Test
+    fun `create new item if id=0 is loaded`() {
+        // arrange
+        val item = FakeTodoModel(text = "new item")
+
+        whenever(todoFactory.createDefault()).thenReturn(item)
+
+        val viewModel = FormViewModel(
+            todoRepository = todoRepo,
+            todoModelFactory = todoFactory,
+            presentationService = presentationService
+        )
+
+        // act
+        viewModel.loadTodoItemToEditForm(0)
+
+        // assert
+        Assert.assertEquals(item, viewModel.todoEditModel.value)
+        verify(todoFactory).createDefault()
+
+    }
+
+    @Test
+    fun `create new item if id not exists`() {
+        runTest {
+
+            // arrange
+            val item = FakeTodoModel(text = "new item")
+
+            whenever(todoFactory.createDefault()).thenReturn(item)
+            whenever(todoRepo.getById(1)).thenReturn(null)
+
+            val viewModel = FormViewModel(
+                todoRepository = todoRepo,
+                todoModelFactory = todoFactory,
+                presentationService = presentationService
+            )
+
+            // act
+            viewModel.loadTodoItemToEditForm(1)
+
+            // assert
+            Assert.assertEquals(item, viewModel.todoEditModel.value)
+            verify(todoRepo).getById(1)
+            verify(todoFactory).createDefault()
         }
     }
 
