484d4d4d13fdad6979f3a25b8584b18e029b0786
slaszu <piotr.flasza@gmail.com>
2025-10-05 15:51:01 +0200

    period wizard | in progress

--- app/src/main/java/pl/slaszu/todoapp/ui/element/form/RepeatDialogModel.kt
+++ app/src/main/java/pl/slaszu/todoapp/ui/element/form/RepeatDialogModel.kt
@@ -1,12 +1,20 @@
 package pl.slaszu.todoapp.ui.element.form
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,6 +26,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import pl.slaszu.todoapp.R
 import pl.slaszu.todoapp.domain.repeat.RepeatType
 
@@ -70,7 +79,113 @@ fun RepeatDialogModel(
 
                     HorizontalDivider()
                 }
+                item {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                type = null
+                            },
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Column {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                RadioButton(
+                                    selected = false,
+                                    onClick = {
+                                        type = null
+                                    }
+                                )
+                                Text(
+                                    text = "other"
+                                )
+                            }
+                            Row {
+                                DropdownExample(initialState = false)
+                            }
+                        }
+                    }
+                }
             }
         }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownExample(initialState: Boolean) {
+
+    val options = List(12) {
+        it.toString()
+    }
+    var expanded by remember { mutableStateOf(false) }
+    var text by remember { mutableStateOf(options[0]) }
+
+
+    Row {
+        // digit
+        OutlinedTextField(
+            // The `menuAnchor` modifier must be passed to the text field to handle
+            // expanding/collapsing the menu on click. A read-only text field has
+            // the anchor type `PrimaryNotEditable`.
+            //modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable).weight(0.1f),
+            value = text,
+            onValueChange = {},
+            //readOnly = true,
+            singleLine = true,
+            label = { Text("digit") },
+            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+            modifier = Modifier.weight(0.3f)
+        )
+
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+            modifier = Modifier.weight(0.7f)
+        ) {
+            OutlinedTextField(
+                // The `menuAnchor` modifier must be passed to the text field to handle
+                // expanding/collapsing the menu on click. A read-only text field has
+                // the anchor type `PrimaryNotEditable`.
+                modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable),
+                value = text,
+                onValueChange = {},
+                singleLine = true,
+                label = { Text("period") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option, style = MaterialTheme.typography.bodyLarge) },
+                        onClick = {
+                            text = option
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+
+    }
+
+}
+
+
+@Composable
+@Preview
+fun RepeatDialogModalPreview() {
+    RepeatDialogModel(
+        selectedType = null,
+        onDismiss = { },
+        onClick = { }
     )
 }
